50234cd215891d3c9d0574698f21aca063958c12
arturazarov <artur.azarau@gmail.com>
2018-01-28 06:17:26 +0300

    Initial Commit

---
+++ BasicSyntaxObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BasicSyntaxObjC
+//
+//  Created by Артур Азаров on 28.01.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
